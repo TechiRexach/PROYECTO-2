25825b6c78dad31e30bcbd961feed77b3e1e013e
Techi Rexach <techi90@gmail.com>
2021-02-11 20:10:31 +0100

    estilos desde css y organizacion js

--- main.js
+++ main.js
@@ -1,64 +1,61 @@
 var local = dataPartidos.matches;
 
-function equiposYResultados(partidos){
+// function equiposYResultados(partidos){
     
 
 
-    for (let i = 0; i < partidos.length; i++){
-        console.log(partidos[i].homeTeam.name);
-        console.log(partidos[i].awayTeam.name);
-        console.log(partidos[i].score.fullTime.homeTeam, " - ", partidos[i].score.fullTime.awayTeam);
-        console.log(partidos[i].utcDate);
+//     for (let i = 0; i < partidos.length; i++){
+//         console.log(partidos[i].homeTeam.name);
+//         console.log(partidos[i].awayTeam.name);
+//         console.log(partidos[i].score.fullTime.homeTeam, " - ", partidos[i].score.fullTime.awayTeam);
+//         console.log(partidos[i].utcDate);
 
-    }
-}
-equiposYResultados(local);
+//     }
+// }
+// equiposYResultados(local);
 
 function crearTabla(partidos){
 
     let tabla = document.getElementById("tabla");
 
-    
-
     for (let i = 0; i < partidos.length; i++){
         const tr = document.createElement("tr");
         tr.style.backgroundColor = "lightgrey";
 
-        let tdteam1 = document.createElement("td");
-            tdteam1.classList.add("estiloteam");
-            tdteam1.textContent = partidos[i].homeTeam.name;
-            let escudoimg = document.createElement("img");
-                escudoimg.classList.add("imgteam1");
-                escudoimg.setAttribute("src", "https://crests.football-data.org/" + partidos[i].homeTeam.id + ".svg");
-                escudoimg.setAttribute("alt", "Escudo Equipo");       
-            tdteam1.appendChild(escudoimg);
-            tr.appendChild(tdteam1);
+        let escudoimg = document.createElement("img");
+            escudoimg.classList.add("imgteam1partidos");
+            escudoimg.setAttribute("src", "https://crests.football-data.org/" + partidos[i].homeTeam.id + ".svg");
+            escudoimg.setAttribute("alt", "Escudo Equipo");   
 
-        let tdResult = document.createElement("td");
-            tdResult.textContent = partidos[i].score.fullTime.homeTeam + " - " + partidos[i].score.fullTime.awayTeam;
-            tdResult.style.textAlign = "center";
-            tr.appendChild(tdResult);
-            
-
-        let tdteam2 = document.createElement("td");
-            tdteam2.classList.add("estiloteam");
-            tdteam2.textContent = partidos[i].awayTeam.name;
-            let escudoimg2 = document.createElement("img");
-                escudoimg2.classList.add("imgteam2");
-                escudoimg2.setAttribute("src", "https://crests.football-data.org/" + partidos[i].awayTeam.id + ".svg");
-                escudoimg2.setAttribute("alt", "Escudo Equipo");     
-            tdteam2.appendChild(escudoimg2);
-            tr.appendChild(tdteam2);  
+        let escudoimg2 = document.createElement("img");
+            escudoimg2.classList.add("imgteam2partidos");
+            escudoimg2.setAttribute("src", "https://crests.football-data.org/" + partidos[i].awayTeam.id + ".svg");
+            escudoimg2.setAttribute("alt", "Escudo Equipo");  
 
         let fecha = new Date (partidos[i].utcDate);
 
-        let tdfecha = document.createElement("td");
-            tdfecha.textContent = fecha.toLocaleString();
-            tdfecha.style.textAlign = "center";
-            tr.appendChild(tdfecha);  
+        let resultados = partidos[i].score.fullTime.homeTeam + " - " + partidos[i].score.fullTime.awayTeam;
+            if (resultados === "null - null"){
+                resultados = "Pendiente";
+            }
+            else{
+                resultados.textContent = partidos[i].score.fullTime.homeTeam + " - " + partidos[i].score.fullTime.awayTeam;
+            }
+            
+        let datosPartidos = [escudoimg,
+            partidos[i].homeTeam.name, 
+            resultados,
+            partidos[i].awayTeam.name, 
+            escudoimg2,
+            fecha.toLocaleString(),
+            ]
 
-        tabla.appendChild(tr);
+            for (let j = 0; j < datosPartidos.length; j++){
+                const td = document.createElement("td");
+                td.append(datosPartidos[j]);
+                tr.appendChild(td);
+                tabla.appendChild(tr);   
+        }
     }
 }
-crearTabla(local)
-
+crearTabla(local);
--- tablaClasificacion.js
+++ tablaClasificacion.js
@@ -2,55 +2,53 @@
 var equipos = dataClasificacion.standings;
 
 
-function clasificacionTemporada (standings){
+// function clasificacionTemporada (standings){
  
-    let arrayTable = standings[0].table;
+//     let arrayTable = standings[0].table;
 
-for ( let i = 0; i < arrayTable.length; i++){
-    console.log(standings[0].table[i].position);
-    console.log(standings[0].table[i].team.name);
-    console.log(standings[0].table[i].team.crestUrl);
-    console.log(standings[0].table[1].playedGames);
-}
-}
-clasificacionTemporada(equipos)
+// for ( let i = 0; i < arrayTable.length; i++){
+//     console.log(standings[0].table[i].position);
+//     console.log(standings[0].table[i].team.name);
+//     console.log(standings[0].table[i].team.crestUrl);
+//     console.log(standings[0].table[1].playedGames);
+// }
+// }
+// clasificacionTemporada(equipos)
+
 
 function crearTabla(standings){
     
     let arrayTable = standings[0].table;
 
     let tabla = document.getElementById("tabla");
-        
+
     for (let i = 0; i < arrayTable.length; i++){
         const tr = document.createElement("tr");
         tr.style.backgroundColor = "lightgrey";
 
-        let escudo = document.createElement("td");
-            escudo.classList.add("tdescudo");
         let escudoimg = document.createElement("img");
-            escudoimg.classList.add("imgteam1");
+            escudoimg.classList.add("imgteam1clasificacion");
             escudoimg.setAttribute("src", "https://crests.football-data.org/" + standings[0].table[i].team.id + ".svg");
             escudoimg.setAttribute("alt", "Escudo Equipo");
-            escudo.appendChild(escudoimg);
-            tr.appendChild(escudo);
 
-        let nombreEquipo = document.createElement("td");
-            nombreEquipo.textContent = standings[0].table[i].team.name;
-            nombreEquipo.style.textAlign = "center";
-            nombreEquipo.style.width = "200px";
-            tr.appendChild(nombreEquipo);
+        let infoTabla = standings[0].table[i];
 
-        let posicionEquipo = document.createElement("td");
-            posicionEquipo.textContent = standings[0].table[i].position;
-            posicionEquipo.style.textAlign = "center";
-            tr.appendChild(posicionEquipo);  
+        let datosTabla = [escudoimg, 
+            infoTabla.team.name, 
+            infoTabla.position,
+            infoTabla.playedGames,
+            infoTabla.won,
+            infoTabla.lost,
+            infoTabla.draw,
+            infoTabla.form];
 
-        let partidosJugados = document.createElement("td");
-            partidosJugados.textContent = standings[0].table[1].playedGames;
-            partidosJugados.style.textAlign = "center";
-            tr.appendChild(partidosJugados); 
-
-        tabla.appendChild(tr);
+        for (let j = 0; j < datosTabla.length; j++){
+            const td = document.createElement("td");
+            td.append(datosTabla[j]);
+            tr.appendChild(td);
+            tabla.appendChild(tr);
+        }  
     }
 }
+
 crearTabla(equipos)
